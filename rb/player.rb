1d8a4a4e0ee1f65b923b199e57f20747ca4588d5
Jon Lemmon <jonlemmon@gmail.com>
2021-10-24 15:17:36 +1300

    Add human class

--- rb/player.rb
+++ rb/player.rb
@@ -5,25 +5,6 @@ class Player < Chingu::GameObject
   attr_reader :score, :x, :y, :direction
 #  traits :collision_detection, :bounding_box
 
-  def setup
-    @image = Gosu::Image.new("assets/characters/knight.png")
-    @shadow = Gosu::Image.new("assets/characters/char_shadow.png")
-    @boom = Gosu::Sample.new("assets/audio/explosion.ogg")
-
-    self.input = {  [:holding_left, :holding_a] => :go_left,
-                    [:holding_right, :holding_d] => :go_right,
-                    [:holding_up, :holding_w] => :go_up,
-                    [:holding_down, :holding_s] => :go_down    }
-
-    @x = @y = 300
-    @vel_x = @vel_y = @angle = 0.0
-    @score = 0
-    @direction = 1
-    @prev_x = 0
-    @prev_y = 0
-    @z = @y
-  end
-
   def get_coordinates
     @prev_x = @x
     @prev_y = @y
@@ -35,14 +16,14 @@ class Player < Chingu::GameObject
     @direction = -1
     collision_x
   end
-  
+
   def go_right
     @vel_x += 1
     @x += 5
     @direction = 1
     collision_x
   end
-  
+
   def go_up
     @vel_y -= 1
     @y -= 5
@@ -70,7 +51,7 @@ class Player < Chingu::GameObject
     @y = @prev_y if check_collisions(@x, @y) == true
   end
 
-  def object_collision  
+  def object_collision
     if check_collisions(@x, @y) == true
       @x = @prev_x
       @y = @prev_y
@@ -94,14 +75,14 @@ class Player < Chingu::GameObject
     @vel_y *= 0.85
     object_collision
   end
-  
+
   def draw
     @image.draw_rot(@x, @y, @y + 10, 0, 0.5, 0.5, @direction * 1.25, 1.25)
     @shadow.draw_rot(@x, @y, @y + 9, 0, 0.5, 0.5, @direction * 1.25, 1.25)
 #draw_rot(x, y, z, angle, center_x = 0.5, center_y = 0.5, scale_x = 1, scale_y = 1)
 #    @image.draw_rot(@x, @y, Z::PLAYER, @angle)
   end
-  
+
 
   def grab_toy
 
