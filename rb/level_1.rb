e1afca52872b06110cad29f1dfd53fbee2efa062
Jon Lemmon <jonlemmon@gmail.com>
2021-10-24 17:30:05 +1300

    Add levels

--- rb/level_1.rb
+++ rb/level_1.rb
@@ -5,8 +5,16 @@
 
 class Level1 < LivingRoom
   def initialize
-    @num_toys = 15
-    @num_kids = 4
+    @num_toys = 2
+    @num_kids = 2
     super
+  end
+
+  def update
+    super
+    if @particles.length < 1
+      puts "YOU'VE WON!"
+      push_game_state(Chingu::GameStates::FadeTo.new(Level2.new, :speed => 8))
+    end
   end
 end
--- rb/level_2.rb
+++ rb/level_2.rb
@@ -9,4 +9,12 @@ class Level2 < LivingRoom
     @num_kids = 14
     super
   end
+
+  def update
+    super
+    if @particles.length < 1
+      puts "YOU'VE WON!"
+      push_game_state(Chingu::GameStates::FadeTo.new(Level3.new, :speed => 8))
+    end
+  end
 end
