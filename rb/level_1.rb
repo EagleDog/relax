6c7ab8edc2a72ef10c265fe4e3968a4c49ddb59a
eagleDog <ethandragoin@gmail.com>
2021-10-23 20:28:07 -0600

    stub in levels and ending

---
+++ rb/level_1.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#                                             #
+#                LEVEL 1                      #
+#                                             #
+
+class Level1 < LivingRoom
+
+
+
+
+end
