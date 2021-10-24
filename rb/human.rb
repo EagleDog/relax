797dd065c68dc748b9c9920758408fe12d9e9595
Jon Lemmon <jonlemmon@gmail.com>
2021-10-24 15:58:47 +1300

    Drop toooooyyyssss

--- rb/human.rb
+++ rb/human.rb
@@ -9,7 +9,8 @@ class Human < Player
     self.input = {  [:holding_left, :holding_a] => :go_left,
                     [:holding_right, :holding_d] => :go_right,
                     [:holding_up, :holding_w] => :go_up,
-                    [:holding_down, :holding_s] => :go_down    }
+                    [:holding_down, :holding_s] => :go_down,
+                    [:holding_space, :holding_enter] => :drop_toys,  }
 
     @x = @y = 300
     @vel_x = @vel_y = @angle = 0.0
@@ -18,5 +19,31 @@ class Human < Player
     @prev_x = 0
     @prev_y = 0
     @z = @y
+    @particles = nil
+  end
+
+  def assign_particles(particles)
+    @particles = particles
+  end
+
+  def collect_toys(particles)
+    particles.each do |particle|
+      if particle.held < -1
+        if Gosu.distance(@x, @y, particle.x, particle.y) < 45
+          @boom.play # if rand(4) == 1
+          particle.held = -1
+        end
+      end
+    end
+  end
+
+  def drop_toys
+    @particles.each do |particle|
+      if particle.held == -1
+        particle.held = -2
+        particle.x += 50 * @direction
+        particle.y += 50 * @direction
+      end
+    end
   end
 end
